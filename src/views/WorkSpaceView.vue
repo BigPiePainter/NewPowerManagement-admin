1ae027b22847638f29ca24ebba8ee66a0ad6c6f6
Nick <nickyoung0017@outlook.com>
2023-06-06 05:34:51 +0800

    try card

--- src/views/WorkSpaceView.vue
+++ src/views/WorkSpaceView.vue
@@ -3,5 +3,42 @@
 </script>
 
 <template>
-  首页首页首页首页首页首页首页首页
+<div>
+  <el-row :gutter="18">
+    <el-col :span="5">
+      <el-card shadow="always"> Always </el-card>
+    </el-col>
+    <el-col :span="5">
+      <el-card shadow="hover"> Hover </el-card>
+    </el-col>
+    <el-col :span="5">
+      <el-card shadow="never"> Never </el-card>
+    </el-col>
+    <el-col :span="5">
+      <el-card shadow="never"> Never </el-card>
+    </el-col>
+  </el-row>
+
+  <el-row :gutter="18">
+    <el-col :span="5">
+      <el-card shadow="always"> Always </el-card>
+    </el-col>
+    <el-col :span="5">
+      <el-card shadow="hover"> Hover </el-card>
+    </el-col>
+  </el-row>
+</div>
 </template>
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+  margin-left: 1vw;
+  margin-right: 1vw;
+}
+</style>
