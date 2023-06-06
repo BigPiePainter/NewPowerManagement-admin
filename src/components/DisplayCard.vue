311bb6e2d0a0f0d6960b62eb4bff596bd5307e4a
Aaron <751479530@qq.com>
2023-06-06 17:55:12 +0800

    display模板

---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+type Props = {
+  title: string
+  amount: number
+}
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+  <div>
+    <div>{{ title }}</div>
+    <div>{{ amount }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/WorkSpaceView.vue
+++ src/views/WorkSpaceView.vue
@@ -1,41 +1,67 @@
 <script setup lang="ts">
+import DisplayCard from '../components/DisplayCard.vue'
+import { ref, reactive } from 'vue';
+
+
+var data = [1, 2, 3]
+
+var data2 = {
+  name: "zhang",
+  age: 16,
+}
+
+//[] 是变量索引
+//.  是字符串索引
+
+
+
+
+
+
+
 
 </script>
 
 <template>
-<div>
-  <el-row :gutter="18">
-    <el-col :span="5">
-      <el-card shadow="always"> Always </el-card>
-    </el-col>
-    <el-col :span="5">
-      <el-card shadow="hover"> Hover </el-card>
-    </el-col>
-    <el-col :span="5">
-      <el-card shadow="never"> Never </el-card>
-    </el-col>
-    <el-col :span="5">
-      <el-card shadow="never"> Never </el-card>
-    </el-col>
-  </el-row>
+  <div>
 
-  <el-row :gutter="18">
-    <el-col :span="5">
-      <el-card shadow="always"> Always </el-card>
-    </el-col>
-    <el-col :span="5">
-      <el-card shadow="hover"> Hover </el-card>
-    </el-col>
-  </el-row>
-</div>
+    <DisplayCard v-for="i in 10" :key="i" title="a" :amount="100"> </DisplayCard>
+
+
+    <el-row :gutter="18">
+      <el-col :span="5">
+        <el-card shadow="always"> Always </el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card shadow="hover"> Hover </el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card shadow="never"> Never </el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card shadow="never"> Never </el-card>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="18">
+      <el-col :span="5">
+        <el-card shadow="always"> Always </el-card>
+      </el-col>
+      <el-col :span="5">
+        <el-card shadow="hover"> Hover </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <style>
 .el-row {
   margin-bottom: 20px;
 }
+
 .el-row:last-child {
   margin-bottom: 0;
 }
+
 .el-col {
   border-radius: 4px;
   margin-left: 1vw;
