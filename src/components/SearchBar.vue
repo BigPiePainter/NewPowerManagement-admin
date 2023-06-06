42bd7ae11cf8a861b6c09cd1a284f047d6578620
Nick <nickyoung0017@outlook.com>
2023-06-07 02:01:25 +0800

    搜索框组件

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,43 +1,80 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue';
+import { Search, Refresh } from '@element-plus/icons-vue'
+
+const emit = defineEmits(['change'])
+
+enum InputType { Input, Select }
+
+interface searchElement {
+  name: string,
+  value: string,
+  type?: InputType;
+}
 
 type Props = {
-  items: any
+  items: searchElement[]
 }
 const props = defineProps<Props>()
-const inputFromSub = ref()
+
+
+const clickSearch = () => {
+  //传递给父组件
+  emit('change', props.items)
+}
+
+const clickRefresh = () => {
+  props.items.forEach(item => {
+    console.log(item)
+    item.value = ""
+    emit('change', props.items)
+  });
+}
 
 </script>
 
 <template>
-  <div>
-    <v-row>
-      <v-col v-for="item in items" :key="item.name">
-        <span>{{ item.name }}</span>
-        <el-input v-model="inputFromSub" />
-      </v-col>
+  <div class="search-bar">
+    <div class="search-element" v-for="item in items" :key="item.name">
+      <el-text class="search-title">{{ item.name }}</el-text>
+      <el-select v-if="item.type == InputType.Select" class="search-input" placeholder="请输入" v-model="item.value" />
+      <el-input v-else class="search-input" placeholder="请输入" v-model="item.value" />
+    </div>
 
-      <v-col>
-        <v-col>
-          <el-button>搜索</el-button>
-        </v-col>
-        <v-col>
-          <el-button>重置</el-button>
-        </v-col>
-      </v-col>
-    </v-row>
+    <div style="flex-grow: 1"></div>
+
+    <el-button @click="clickSearch" class="button" :icon="Search">搜索</el-button>
+    <el-button @click="clickRefresh" class="button" :icon="Refresh">重置</el-button>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
 
-<style>
-.el-row {
-  margin-top: 2vw;
-}
 
-.el-col {
-  margin-left: 1vw;
-  margin-right: 1vw;
+<style scoped lang="scss">
+.search-bar {
+  display: flex;
+
+  >.search-element {
+    display: flex;
+    align-items: center;
+
+    >.search-title {
+      margin-right: 8px;
+      white-space: nowrap;
+    }
+
+    >.search-input {
+      max-width: 300px;
+      max-height: 30px;
+      margin-right: 12px;
+    }
+  }
+
+  >.button {
+    margin-top: 1px;
+    max-height: 30px;
+  }
+
+
 }
 </style>
