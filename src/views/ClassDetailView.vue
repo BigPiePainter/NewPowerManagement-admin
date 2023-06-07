6e96b6897fa5b399aa4891d1d66d5d7c59c3b30c
Nick <nickyoung0017@outlook.com>
2023-06-07 23:54:50 +0800

    班级详情页跳转

---
+++ src/views/ClassDetailView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.search-bar {
+  margin-top: 15px;
+  margin-left: 17px;
+  margin-right: 15px;
+}
+
+.cell {
+  margin-right: 10px;
+}
+
+.table-class-management {
+  margin-top: 15px;
+  margin-left: 15px;
+  padding-bottom: 20px;
+  height: calc(100vh - 200px);
+  width: calc(100vw - $sidebar-width - 15px);
+  box-sizing: border-box;
+}
+
+.new-class-button {
+  margin-top: 15px;
+  margin-left: 15px;
+  max-height: 30px;
+}
+</style>
