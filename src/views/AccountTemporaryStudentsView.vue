9eb6eea0cd43bf2f23378974925b6fff3ad7d63a
Zack <zqy21@qq.com>
2023-06-09 00:39:07 +0800

    修改样式

--- src/views/AccountTemporaryStudentsView.vue
+++ src/views/AccountTemporaryStudentsView.vue
@@ -5,38 +5,35 @@ import SearchBar from '@/components/SearchBar.vue'
 import TablePage from '@/components/TablePage.vue'
 import { InputType } from '@/type'
 
-
 const items = reactive([
-  { name: "姓名", value: "" },
-  { name: "手机", value: ""}
-
+  { name: '姓名', value: '' },
+  { name: '手机', value: '' }
 ])
-
 
 const tableColumns = [
   {
     dataKey: 'id',
     key: 'id',
     title: 'ID',
-    width: 200,
+    width: 200
   },
   {
     dataKey: 'studentName',
     key: 'studentName',
     title: '姓名',
-    width: 200,
+    width: 200
   },
   {
     dataKey: 'phoneNumber',
     key: 'phoneNumber',
     title: '手机号',
-    width: 200,
+    width: 200
   },
   {
     dataKey: 'school',
     key: 'school',
     title: '学校',
-    width: 200,
+    width: 200
   },
   {
     key: 'option',
@@ -57,72 +54,55 @@ const tableColumns = [
         </el-button>
       </>
     ),
-    
+
     width: 300,
-  fixed: 'right',
+    fixed: 'right',
     align: 'center'
   }
 ]
-
 
 let fakeData = {
   id: '1',
   studentName: 'Aaron',
   school: '上虞中学',
   phoneNumber: '155799928891',
-  createTime: "2011-9-19 18:23",
+  createTime: '2011-9-19 18:23'
 }
 
 const tableData: object[] = []
 
 for (let index = 0; index < 100; index++) {
   let data = { ...fakeData }
-  data.id += index;
+  data.id += index
   tableData.push(data)
 }
-
-
-
 
 console.log(tableData)
 
 const refresh = () => {
   console.log(items)
 }
-
 </script>
 
-
 <template>
-  <div class="div-search-bar ">
-    <SearchBar :items="items" @change="refresh()"></SearchBar>
-
-  </div>
-  <TablePage class="table-page table" :columns="tableColumns" :data="tableData">
+  <TablePage class="page-container" :columns="tableColumns" :data="tableData">
+    <div class="div-search-bar">
+      <SearchBar :items="items" @change="refresh()"></SearchBar>
+    </div>
   </TablePage>
 </template>
 
 <style scoped lang="scss">
-.table {
-  // margin-left: 15px;
-  // margin-top: 15px;
+$gap: 15px;
 
-  width: $page-width;
-  height: 82.5vh;
+.page-container {
+  width: calc($page-width - $gap);
+  height: $page-height;
+  margin-left: $gap;
+  //margin-right: $gap;
 }
 
-
 .div-search-bar {
-  display: flex;
-  margin-top: 15px;
-  margin-bottom: 15px;
-  margin-right: 15px;
-
-  >.search-bar-button {
-    max-width: 70px;
-    max-height: 30px;
-    margin-left: 12px;
-  }
-
+  margin: $gap;
 }
 </style>
