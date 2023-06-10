342132346e12ea4bd2f3e7f2fca1a0c68e799b22
Nick <nickyoung0017@outlook.com>
2023-06-10 11:51:41 +0800

    班级管理和教研组管理dialog以及相应逻辑

--- src/views/ClassManagementView.vue
+++ src/views/ClassManagementView.vue
@@ -21,9 +21,31 @@ const items = reactive([
   { name: "学科", value: "" },
 ])
 
-const dialogShow = ref(false);
+const newClassDialogShow = ref(false);
+const newClassData = reactive<{
+  className: string,
+  teacher: string,
+  startDate: string,
+  endDate: string,
+  major: string,
+  grade: string,
+}>({
+  className: '',
+  teacher: '',
+  startDate: '',
+  endDate: '',
+  major: '',
+  grade: '',
+});
 const creatNewClass = () => {
-  dialogShow.value = true;
+  newClassDialogShow.value = true;
+}
+const confirmNewClass = () => {
+  console.log(newClassData)
+  newClassDialogShow.value = false
+}
+const cancelNewClass = () => {
+  newClassDialogShow.value = false
 }
 
 const tableData = reactive<object[]>([
@@ -34,7 +56,7 @@ const tableData = reactive<object[]>([
     grade: '9年级',
     major: '语文',
     studentNum: '20',
-    untilDate: '2021-12-02',
+    endDate: '2021-12-02',
   }
 ])
 
@@ -47,7 +69,7 @@ const pushData = () => {
       grade: '9年级',
       major: '语文',
       studentNum: '20',
-      untilDate: '2021-12-02',
+      endDate: '2021-12-02',
     }
     tableData.push(fakeData)
   }
@@ -99,8 +121,8 @@ const tableColumns = reactive([
     width: 150
   },
   {
-    dataKey: 'untilDate',
-    key: 'untilDate',
+    dataKey: 'endDate',
+    key: 'endDate',
     title: '到期时间',
     width: 150
   },
@@ -110,8 +132,8 @@ const tableColumns = reactive([
     cellRenderer: (item: any) => {
       return (
         <div>
-          <ElButton link type='primary' onClick={() => editItem(item)}>编辑</ElButton>
-          <ElButton link type='primary' onClick={() => deleteItem(item)}>删除</ElButton>
+          <ElButton link type='primary' onClick={() => editClass(item)}>编辑</ElButton>
+          <ElButton link type='primary' onClick={() => deleteClass(item)}>删除</ElButton>
         </div>
       )
     },
@@ -125,12 +147,63 @@ const clickDetail = (props: { rowData: { id: string } }) => {
   router.push({ path: 'class-detail', query: { id: props.rowData.id } });
 }
 
-const editItem = (props: object) => {
+const editClassDialogShow = ref(false);
+const editClassData = reactive<{
+  className: string,
+  teacher: string,
+  startDate: string,
+  endDate: string,
+  major: string,
+  grade: string,
+}>({
+  className: '',
+  teacher: '',
+  startDate: '',
+  endDate: '',
+  major: '',
+  grade: '',
+});
+const editClass = (props: {
+  rowData: {
+    className: string,
+    teacher: string,
+    startDate: string,
+    endDate: string,
+    major: string,
+    grade: string,
+  }
+}) => {
   console.log(props);
+  editClassDialogShow.value = true;
+  editClassData.className = props.rowData.className;
+  editClassData.teacher = props.rowData.teacher;
+  editClassData.startDate = props.rowData.startDate;
+  editClassData.endDate = props.rowData.endDate;
+  editClassData.major = props.rowData.major;
+  editClassData.grade = props.rowData.grade;
 }
 
-const deleteItem = (props: object) => {
+const deleteClassDialogShow = ref(false);
+const deleteClassData = reactive<{ className: string }>({ className: '' });
+const deleteClass = (props: { rowData: { id:string, className: string}}) => {
   console.log(props);
+  deleteClassDialogShow.value = true;
+  deleteClassData.className = props.rowData.className
+}
+
+const confirmEditDialog = () => {
+  console.log(editClassData)
+  editClassDialogShow.value = false;
+}
+const cancelEditDialog = () => {
+  editClassDialogShow.value = false;
+}
+const confirmDeleteDialog = () => {
+  console.log(deleteClassData);
+  deleteClassDialogShow.value = false;
+}
+const cancelDeleteDialog = () => {
+  deleteClassDialogShow.value = false;
 }
 
 const refresh = () => {
@@ -150,54 +223,48 @@ const refresh = () => {
     </TablePage>
   </div>
 
-  <el-dialog class="new-class-dialog" width="370px" v-model="dialogShow">
+  <el-dialog class="new-class-dialog" width="370px" v-model="newClassDialogShow">
     <div>
       <div class="div-input-element">
         <span class="dialog-span">
           *班级名称：
         </span>
-        <el-input class="dialog-input" placeholder="请输入">
-
+        <el-input class="dialog-input" placeholder="请输入" v-model="newClassData.className">
         </el-input>
       </div>
       <div class="div-input-element">
         <span class="dialog-span">
           *负责老师：
         </span>
-        <el-input class="dialog-input" placeholder="请输入">
-
+        <el-input class="dialog-input" placeholder="请输入" v-model="newClassData.teacher">
         </el-input>
       </div>
       <div class="div-input-element">
         <span class="dialog-span">
           *起始时间：
         </span>
-        <el-input class="dialog-input" placeholder="请输入">
-
+        <el-input class="dialog-input" placeholder="请输入" v-model="newClassData.startDate">
         </el-input>
       </div>
       <div class="div-input-element">
         <span class="dialog-span">
           *到期时间：
         </span>
-        <el-input class="dialog-input" placeholder="请输入">
-
+        <el-input class="dialog-input" placeholder="请输入" v-model="newClassData.endDate">
         </el-input>
       </div>
       <div class="div-input-element">
         <span class="dialog-span">
           学科：
         </span>
-        <el-input class="dialog-input" placeholder="请输入">
-
+        <el-input class="dialog-input" placeholder="请输入" v-model="newClassData.major">
         </el-input>
       </div>
       <div class="div-input-element">
         <span class="dialog-span">
           年级：
         </span>
-        <el-input class="dialog-input" placeholder="请输入">
-
+        <el-input class="dialog-input" placeholder="请输入" v-model="newClassData.grade">
         </el-input>
       </div>
     </div>
@@ -205,8 +272,79 @@ const refresh = () => {
       <el-text>新建班级</el-text>
     </template>
     <template #footer>
-      <el-button type="primary">确定</el-button>
-      <el-button>
+      <el-button type="primary" @click="confirmNewClass()">确定</el-button>
+      <el-button @click="cancelNewClass()">
+        取消
+      </el-button>
+    </template>
+  </el-dialog>
+
+  <el-dialog class="new-class-dialog" width="370px" v-model="editClassDialogShow">
+    <div>
+      <div class="div-input-element">
+        <span class="dialog-span">
+          *班级名称：
+        </span>
+        <el-input class="dialog-input" v-model="editClassData.className">
+        </el-input>
+      </div>
+      <div class="div-input-element">
+        <span class="dialog-span">
+          *负责老师：
+        </span>
+        <el-input class="dialog-input" v-model="editClassData.teacher">
+        </el-input>
+      </div>
+      <div class="div-input-element">
+        <span class="dialog-span">
+          *起始时间：
+        </span>
+        <el-input class="dialog-input" v-model="editClassData.startDate">
+        </el-input>
+      </div>
+      <div class="div-input-element">
+        <span class="dialog-span">
+          *到期时间：
+        </span>
+        <el-input class="dialog-input" v-model="editClassData.endDate">
+        </el-input>
+      </div>
+      <div class="div-input-element">
+        <span class="dialog-span">
+          学科：
+        </span>
+        <el-input class="dialog-input" v-model="editClassData.major">
+        </el-input>
+      </div>
+      <div class="div-input-element">
+        <span class="dialog-span">
+          年级：
+        </span>
+        <el-input class="dialog-input" v-model="editClassData.grade">
+        </el-input>
+      </div>
+    </div>
+    <template #header>
+      <el-text>编辑班级</el-text>
+    </template>
+    <template #footer>
+      <el-button type="primary" @click="confirmEditDialog()">确定</el-button>
+      <el-button @click="cancelEditDialog()">
+        取消
+      </el-button>
+    </template>
+  </el-dialog>
+
+  <el-dialog class="new-class-dialog" width="370px" v-model="deleteClassDialogShow">
+    <template #header>
+      <el-text>确定删除班级</el-text>
+    </template>
+    <span>
+      {{ deleteClassData.className }}
+    </span>
+    <template #footer>
+      <el-button type="danger" @click="confirmDeleteDialog()">确定</el-button>
+      <el-button @click="cancelDeleteDialog()">
         取消
       </el-button>
     </template>
