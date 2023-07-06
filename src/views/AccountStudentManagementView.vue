dee36a1e2e18d320da9a992201f0ab44d957120b
Zack <zqy21@qq.com>
2023-07-06 19:01:46 +0800

    优化

--- src/views/AccountStudentManagementView.vue
+++ src/views/AccountStudentManagementView.vue
@@ -8,7 +8,6 @@ import { useBreadcrumbStore } from '@/stores/breadcrumb'
 import { useRouter } from 'vue-router'
 import { getStudent } from '@/apis/studentManagement'
 
-
 const router = useRouter()
 
 const studentName = (props: any) => {
@@ -20,15 +19,13 @@ const breadcrumbStore = useBreadcrumbStore()
 
 breadcrumbStore.data = [
   { name: '账号管理', path: '' },
-  { name: '学生管理', path: '/account-student-management' },
+  { name: '学生管理', path: '/account-student-management' }
 ]
 const searchBarItemsitems = reactive([
-  { name: "用户名", value: "" },
-  { name: "姓名", value: "", },
-  { name: "电话", value: "" },
-
+  { name: '用户名', value: '' },
+  { name: '姓名', value: '' },
+  { name: '电话', value: '' }
 ])
-
 
 const tableColumns = [
   {
@@ -38,64 +35,68 @@ const tableColumns = [
     width: 60
   },
   {
-    dataKey: 'studentName',
-    key: 'studentName',
+    dataKey: 'name',
+    key: 'name',
     title: '姓名',
     width: 80,
-    cellRenderer: (cellData: any) => <ElButton link type='primary' onClick={() => studentName(cellData)}>{cellData.cellData}</ElButton>,
+    cellRenderer: (cellData: any) => (
+      <ElButton link type="primary" onClick={() => studentName(cellData)}>
+        {cellData.cellData}
+      </ElButton>
+    )
   },
   {
-    dataKey: 'userName',
-    key: 'userName',
+    dataKey: 'account',
+    key: 'account',
     title: '用户名',
     width: 170
   },
   {
-    dataKey: 'expiryDate',
-    key: 'expiryDate',
+    dataKey: 'expiration',
+    key: 'expiration',
     title: '有效期',
     width: 140
   },
   {
-    dataKey: 'studentGrade',
-    key: 'Studentgrade',
+    dataKey: 'gradeName',
+    key: 'gradeName',
     title: '年级',
     width: 70
   },
   {
-    dataKey: 'studentCellnumber',
-    key: 'studentCellnumber',
+    dataKey: 'phoneNumber',
+    key: 'phoneNumber',
     title: '手机号码',
     width: 200
   },
   {
-    dataKey: 'gender',
-    key: 'gender',
+    dataKey: 'sexName',
+    key: 'sexName',
     title: '性别',
     width: 70
   },
   {
-    dataKey: 'parentCellnumber',
-    key: 'parentCellnumber',
+    dataKey: 'phoneNumberOfParent',
+    key: 'phoneNumberOfParent',
     title: '家长手机号码',
     width: 180
   },
   {
-    dataKey: 'note',
-    key: 'note',
+    dataKey: 'remark',
+    key: 'remark',
     title: '备注',
     width: 120
   },
   {
-    dataKey: 'createTime',
-    key: 'createTime',
+    dataKey: 'createdAt',
+    key: 'createdAt',
     title: '创建时间',
     width: 200
   },
 
   {
-    dataKey: 'loginTime',
-    key: 'loginTime',
+    dataKey: 'lastLoginTime',
+    key: 'lastLoginTime',
     title: '最后登陆时间',
     width: 200
   },
@@ -124,9 +125,7 @@ const tableColumns = [
   }
 ]
 
-
-
-const tableData = reactive([{}])
+const tableData = ref<object[]>([])
 
 const showDialog = ref(false)
 
@@ -142,9 +141,9 @@ const cancel = () => {
 const selectOptionGrades = reactive<any>([])
 
 const searchBarItems = reactive([
-  { name: "用户名", value: "" },
-  { name: "姓名", value: "" },
-  { name: "手机号", value: "", label: "" },
+  { name: '用户名', value: '' },
+  { name: '姓名', value: '' },
+  { name: '手机号', value: '', label: '' }
 ])
 
 const paginationInfo = reactive({
@@ -153,53 +152,12 @@ const paginationInfo = reactive({
   type: 1
 })
 
-
 const dataCompute = (items: any) => {
-  tableData.length = 0
-  items.data.records.forEach((item: any) => {
-    var dataSample = {
-      id: '',
-      studentName: '',
-      userName: '',
-      studentCellnumber: '',
-      note: '',
-      gender: '',
-      expiryDate: '',
-      parentCellnumber: '',
-      studentGrade: '',
-
-    }
-
-
-    dataSample.id = item.id
-    dataSample.studentName = item.name
-    dataSample.userName = item.account
-    dataSample.studentCellnumber = item.phoneNumber
-    dataSample.note = item.remark
-    
-
-    if (item.sex == 1) {
-      item.sex = '男'
-    }
-    else {
-      item.sex = '女'
-    }
-    //1为男生 2为女生 
-
-    dataSample.gender = item.sex
-
-
-    dataSample.expiryDate = item.expiration
-    dataSample.parentCellnumber = item.phoneNumberOfParent
-    dataSample.studentGrade = item.grade
-
-
-    tableData.push(dataSample)
-  });
-  console.log(tableData)
+  items.forEach((i: any) => {
+    i.sexName = i.sex == 1 ? '男' : '女'
+  })
+  tableData.value = items
 }
-
-
 
 const totalLength = ref<Number>()
 
@@ -214,11 +172,13 @@ const loadData = (prop: any) => {
     name: searchBarItems[0].value,
     phoneNumber: searchBarItems[1].value
   }
-  getStudent(args).then((res) => {
-    dataCompute(res)
-    totalLength.value = res.data.records.length
-  })
-    .catch();
+  getStudent(args)
+    .then((res) => {
+      console.log('原始返回', res)
+      dataCompute(res.data.records)
+      totalLength.value = res.data.records.length
+    })
+    .catch()
 }
 
 loadData(paginationInfo)
@@ -229,25 +189,29 @@ const refresh = () => {
 }
 </script>
 
-
 <template>
-  <TablePage class="page-container" :msg="totalLength" @paginationChange="loadData" :columns="tableColumns"
-    :data="tableData">
+  <TablePage
+    class="page-container"
+    :msg="totalLength"
+    @paginationChange="loadData"
+    :columns="tableColumns"
+    :data="tableData"
+  >
     <div class="div-search-bar">
-      <SearchBar :items="searchBarItems" :selectOptions="selectOptionGrades" @change="refresh"></SearchBar>
+      <SearchBar
+        :items="searchBarItems"
+        :selectOptions="selectOptionGrades"
+        @change="refresh"
+      ></SearchBar>
       <el-button class="ARMbutton" type="primary" @click="createStudent">新建学生</el-button>
     </div>
-
   </TablePage>
-
-
 
   <el-dialog v-model="showDialog" width="370px">
     <template #header>
       <el-text>新建学生</el-text>
     </template>
-    <div style="height: 300px;">
-
+    <div style="height: 300px">
       <div class="input">
         <div class="input-word">*用户名:</div>
         <ElInput class="input-input" placeholder="请输入" />
@@ -277,7 +241,6 @@ const refresh = () => {
       <ElButton @click="confrom">取消</ElButton>
 
       <ElButton type="primary" @click="cancel">确认</ElButton>
-
     </template>
   </el-dialog>
 </template>
@@ -305,8 +268,6 @@ $gap: 15px;
   flex-direction: row;
   width: 320px;
   margin-bottom: 10px;
-
-
 }
 
 .input-word {
@@ -315,8 +276,6 @@ $gap: 15px;
   align-items: center;
   width: 80px;
   padding-right: 10px;
-
-
 }
 
 .input-input {
