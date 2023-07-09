97b741de04ff6acbff23ad18b1cb5e7846a2c981
Zack <zqy21@qq.com>
2023-07-09 18:26:37 +0800

    规范化修改

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,99 +2,81 @@
 import { RouterLink } from 'vue-router'
 import { userLogin } from '@/apis/login'
 import { ref, reactive, pushScopeId } from 'vue'
-import { rejects } from 'assert';
-import router from '@/router';
+import { rejects } from 'assert'
+import router from '@/router'
 import { ElNotification } from 'element-plus'
 import { text } from 'stream/consumers'
-import { userInfo } from '@/apis/manager';
+import { userInfo } from '@/apis/manager'
 
-
-
-const account = ref("")
-const password = ref("")
-
+const account = ref('')
+const password = ref('')
 
 const pars = reactive({
-  "account": account.value,
-  "password": password.value,
-  "device": {
-    "deviceId": "Web",
-    "deviceModel": "Web",
-    "deviceName": "Web",
-    "version": 1
-  },
+  account: account.value,
+  password: password.value,
+  device: {
+    deviceId: 'Web',
+    deviceModel: 'Web',
+    deviceName: 'Web',
+    version: 1
+  }
 })
 
-
-
 const enterAccount = () => {
-  pars.account = "15824136350"
-  pars.password = "482c811da5d5b4bc6d497ffa98491e38"
+  pars.account = '15824136350'
+  pars.password = '482c811da5d5b4bc6d497ffa98491e38'
 }
-
 
 const enterHome = () => {
   router.push({ path: 'work-space' })
-
 }
 
 const login = () => {
-  userLogin(pars).then((res) => {
-    if (res.data.code == 20000) {
-      {
+  userLogin(pars)
+    .then((res) => {
+      console.log("THEN", res)        
+      if (res.data.code == 20000) {         //res.data.code   undifined         res.data.code - 报错 - catch
         ElNotification({
           title: 'Warning',
           message: '登入信息错误',
-          type: 'warning',
+          type: 'warning'
         })
-        return;
-      }
-    }
-    else {
-      localStorage.token = res.data.token
-      {
+        return
+      } else {
+        localStorage.token = res.data.token
         ElNotification({
           title: 'Success',
           message: '登陆成功',
-          type: 'success',
+          type: 'success'
         })
+        enterHome()
       }
-      enterHome()
-    }
-  }
-
-  ).catch(() => {
-  {
-        ElNotification({
-          title: 'Warning',
-          message: '网络不佳',
-          type: 'warning',
-        })
-  }
-  
-  }
-  )
+    })
+    .catch((error) => {
+      console.log(error)
+      ElNotification({
+        title: 'Warning',
+        message: '网络不佳',
+        type: 'warning'
+      })
+    })
 }
 </script>
-
-
-
 
 <template>
   <div class="login">
     <el-text class="title">锦鲤项目</el-text>
     <div class="account">
-      <el-text class="word">账号：</el-text> <el-input placeholder="" v-model='pars.account' />
+      <el-text class="word">账号：</el-text> <el-input placeholder="" v-model="pars.account" />
     </div>
     <div class="account">
-      <el-text class="word">密码：</el-text> <el-input placeholder="" v-model='pars.password' show-password />
+      <el-text class="word">密码：</el-text>
+      <el-input placeholder="" v-model="pars.password" show-password />
     </div>
     <el-button type="primary" @click="login">登陆</el-button>
-    <el-button style="margin-top: 10px;" type="primary" @click="enterAccount">烦人，立马登录</el-button>
-
-
-
-
+    <el-button style="margin-top: 10px" type="primary" @click="enterAccount"
+      >烦人，立马登录</el-button
+    >
   </div>
 </template>
 
@@ -118,7 +100,6 @@ const login = () => {
   margin: 8px;
   flex-direction: row;
 }
-
 
 .word {
   display: flex;
